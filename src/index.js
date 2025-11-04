f4be672ef9e039e767b290b7cdfda80d825b85a5
Manoj Prasad <manoj_ca@yahoo.com>
2025-11-04 11:47:28 -0700

    added loaders for images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-// src/index.js
+
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import odinImage from "./images/odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
 
 console.log(greeting);
